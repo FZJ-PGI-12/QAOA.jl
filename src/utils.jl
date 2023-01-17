586a9ad96803aecd5ba5607e8dc3f3e16a705d61
timbode <lappet@protonmail.com>
2023-01-17 10:56:10 +0100

    wrapper for MaxCut

--- src/utils.jl
+++ src/utils.jl
@@ -10,8 +10,39 @@ function partition_problem()
     0
 end
 
-function max_cut()
-    0
+"""
+    max_cut(graph::PyObject; num_layers::Int=1, driver=X)
+
+Wrapper function setting up an instance for the MaxCut problem of the graph `graph`.
+
+### Input
+- `graph::PyObject`: The input graph, must be a Python NetworkX graph.
+- `num_layers::Int=1` (optional): The number of QAOA layers usually denoted by ``p``.
+- `driver=X`` (optional): The driver or mixer used in the QAOA.
+
+### Output
+- An instance of the `Problem` struct holding all relevant quantities.
+
+### Notes
+The cost function for the MaxCut problem as defined in the [original QAOA paper](https://arxiv.org/abs/1411.4028) is
+
+``
+    \\hat C = \\frac{1}{2} \\sum_{(i, j) \\in E(G)} (1 - \\hat Z_i \\hat Z_j),
+``
+    
+where ``E(G)`` is the set of edges of the graph ``G``.
+"""
+function max_cut(graph::PyObject; num_layers::Int=1, driver=X)
+    @assert pybuiltin(:isinstance)(graph, (nx.Graph)) "Input must be a Python NetworkX graph."
+
+    N = graph.number_of_nodes()
+    h = zeros(N)
+    J = zeros(N, N)
+    for edge in graph.edges
+        J[(edge .+ (1, 1))...] = -1/2.
+    end
+    
+    Problem(num_layers, h, J, driver)
 end
 
 
@@ -32,7 +63,7 @@ Wrapper function setting up a problem instance for the minimum vertex cover of t
 The cost function for the minimum-vertex-cover problem is 
 
 ``
-\hat C = -\frac{3}{4} \sum_{(i, j) \in E(G)} (\hat Z_i \hat Z_j  +  \hat Z_i  +  \hat Z_j)  + \sum_{i \in V(G)} \hat Z_i,
+\\hat C = -\\frac{3}{4} \\sum_{(i, j) \\in E(G)} (\\hat Z_i \\hat Z_j  +  \\hat Z_i  +  \\hat Z_j)  + \\sum_{i \\in V(G)} \\hat Z_i,
 ``
 
 where ``E(G)`` is the set of edges and ``V(G)`` is the set of vertices of `graph` (we have a global minus sign since we _maximize_ the cost function).
