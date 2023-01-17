fedcdc2bfdd980518a5a3b70472cc1bbf59e8cfc
timbode <lappet@protonmail.com>
2023-01-17 10:32:05 +0100

    add docstring

--- src/utils.jl
+++ src/utils.jl
@@ -14,6 +14,29 @@ function max_cut()
     0
 end
 
+
+"""
+    min_vertex_cover(graph::PyObject; num_layers::Int=1, driver=X)
+
+Wrapper function setting up a problem instance for the minimum vertex cover of the graph `graph`.
+
+### Input
+- `graph::PyObject`: The input graph, must be a Python NetworkX graph.
+- `num_layers::Int=1` (optional): The number of QAOA layers usually denoted by ``p``.
+- `driver=X`` (optional): The driver or mixer used in the QAOA.
+
+### Output
+- An instance of the `Problem` struct holding all relevant quantities.
+
+### Notes
+The cost function for the minimum-vertex-cover problem is 
+
+``
+\hat C = -\frac{3}{4} \sum_{(i, j) \in E(G)} (\hat Z_i \hat Z_j  +  \hat Z_i  +  \hat Z_j)  + \sum_{i \in V(G)} \hat Z_i,
+``
+
+where ``E(G)`` is the set of edges and ``V(G)`` is the set of vertices of `graph` (we have a global minus sign since we _maximize_ the cost function).
+"""
 function min_vertex_cover(graph::PyObject; num_layers::Int=1, driver=X)
     @assert pybuiltin(:isinstance)(graph, (nx.Graph)) "Input must be a Python NetworkX graph."
 
@@ -27,5 +50,6 @@ function min_vertex_cover(graph::PyObject; num_layers::Int=1, driver=X)
         J[(edge .+ (1, 1))...] = 3/4.
     end    
 
+    # note minus signs (necessary when maximizing)
     Problem(num_layers, -h, -J, driver)
 end
