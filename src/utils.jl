9c65595bda19f86f7d70c4856f86d2a619ba8ab9
timbode <lappet@protonmail.com>
2023-01-17 14:28:05 +0100

    wrapper for Sherrington-Kirkpatrick model

--- src/utils.jl
+++ src/utils.jl
@@ -2,8 +2,37 @@ using PyCall
 np = pyimport("numpy")
 nx = pyimport("networkx")
 
-function sherrington_kirkpatrick()
-    0
+"""
+sherrington_kirkpatrick(variance::Float64; seed::Float64=1.0, num_layers::Int=1, driver=X)
+
+Wrapper function setting up an instance of the Sherrington-Kirkpatrick model.
+
+### Input
+- `N::Int`: The number of spins of the problem.
+- `variance::Float64`: The variance of the distribution of the coupling matrix.
+- 'seed::Float64=1.0': The seed for the random-number generator used in the coupling matrix.
+- `num_layers::Int=1` (optional): The number of QAOA layers usually denoted by ``p``.
+- `driver=X` (optional): The driver or mixer used in the QAOA.
+
+### Output
+- An instance of the `Problem` struct holding all relevant quantities.
+
+### Notes
+The cost function of the Sherrington-Kirkpatrick model is
+
+``
+\\hat H_P = \\frac{1}{\\sqrt{N}}\\sum_{i<j\\leq N} J_{ij} \\hat{Z}_i \\hat{Z}_j,
+``
+
+where the couplings ``J_{ij}`` are i.i.d. standard Gaussian variables, 
+i.e. with zero mean ``\\langle J_{ij} \\rangle = 0`` and variance `` \\langle J_{ij}^2 \\rangle = J^2``.
+"""
+function sherrington_kirkpatrick(N::Int, variance::Float64; seed::Int=1, num_layers::Int=1, driver=X)
+    np.random.seed(seed)
+    J = np.random.normal(0, variance, size=(N, N)) ./ sqrt(N)
+    J = np.triu(J, k=1)
+    J = J + transpose(J)
+    QAOA.Problem(num_layers, zeros(N), J)
 end
 
 
