fd8e45cf2c8be765525c47940542c4153e04e059
timbode <lappet@protonmail.com>
2023-01-17 10:05:51 +0100

    wrapper for min_vertex_cover

--- src/utils.jl
+++ src/utils.jl
@@ -1,3 +1,7 @@
+using PyCall
+np = pyimport("numpy")
+nx = pyimport("networkx")
+
 function sherrington_kirkpatrick()
     0
 end
@@ -10,6 +14,18 @@ function max_cut()
     0
 end
 
-function min_vertex_cover()
-    0
+function min_vertex_cover(graph::PyObject; num_layers::Int=1, driver=X)
+    @assert pybuiltin(:isinstance)(graph, (nx.Graph)) "Input must be a Python NetworkX graph."
+
+    N = graph.number_of_nodes()
+    h = -ones(N)
+    J = zeros(N, N)
+    
+    for edge in graph.edges
+        h[edge[1] + 1] += 3/4.
+        h[edge[2] + 1] += 3/4.
+        J[(edge .+ (1, 1))...] = 3/4.
+    end    
+
+    Problem(num_layers, -h, -J, driver)
 end
