64ad7f869040774210ce382fe6e3c4453520effc
Tim <9047234+timbode@users.noreply.github.com>
2023-01-25 11:01:59 +0100

    1 add paper to repo (#2)
    * add paper
    
    * update notebooks
    
    * add bib file
    
    * update paper
    
    * add file
    
    * update paper
    
    * update paper
    
    * update drivers
    
    * update driver types
    
    * update tests
    
    * add file
    
    * update paper
    
    * benchmark times
    
    * finished benchmarks
    
    * add plot
    
    * add plot
    
    * first draft done
    
    Co-authored-by: Tim Bode <t.bode@Tims-MacBook-Pro.local>
    Co-authored-by: Tim Bode <t.bode@tims-mbp.speedport_w_724v_typ_a_05011603_06_003>

--- src/circuit.jl
+++ src/circuit.jl
@@ -15,7 +15,6 @@ end
     coupling_gates(num_qubits::Int, operators=[Z, Z])
 
 Returns the two-qubit rotation gates corresponding to the coupling term of the Hamiltonian. 
-Alternatively, this function is used to construct two-qubit drivers such as ``\\hat X_i \\hat X_j``.
 
 ### Notes
 - The gates are initialized with trivial parameters, which are then overwritten explicitly elsewhere.
@@ -24,6 +23,18 @@ function coupling_gates(num_qubits::Int, operators=[Z, Z])
     return [put((i, j) => RotationGate(kron(operators...), 0.))(num_qubits) for j in 1:num_qubits for i in 1:j-1] |> chain
 end
 
+
+"""
+    coupling_gates(num_qubits::Int, operators=[Z, Z])
+
+This function is used to construct two-qubit drivers such as ``\\hat X_i \\hat X_j``.
+
+### Notes
+- The gates are initialized with trivial parameters, which are then overwritten explicitly elsewhere.
+"""
+function coupling_gates(edges, num_qubits::Int, operators)
+    return [put(edge => RotationGate(kron(operators...), 0.))(num_qubits) for edge in edges] |> chain
+end
 
 """
     objective_function(num_qubits::Int)    
@@ -40,7 +51,8 @@ end
 
 Returns the gates for one driver layer constructed from a Pauli matrix, e.g. the standard driver has `G==X`.
 """
-function driver(num_qubits::Int, G::YaoBlocks.PauliGate)
+function driver(problem::Problem, G::YaoBlocks.PauliGate)
+    @unpack_Problem problem
     local_field_gates(num_qubits, G)
 end
 
@@ -49,22 +61,26 @@ end
     driver(num_qubits::Int, G::Vector{<:YaoBlocks.PauliGate})
 
 Returns the gates for one driver layer constructed from the Kronecker product of two Pauli matrices, 
-e.g., `G==[X, X]` corresponds to drivers ``\\hat X_i \\hat X_j`` on all pairs of qubits.
+e.g., `G==[X, X]` corresponds to drivers ``\\hat X_i \\hat X_j`` on all pairs of connected qubits.
 """
-function driver(num_qubits::Int, G::Vector{<:YaoBlocks.PauliGate})
+function driver(problem::Problem, G::Vector{<:YaoBlocks.PauliGate})
+    @unpack_Problem problem
     @assert size(G)[1] == 2
-    coupling_gates(num_qubits, G)
+    edges = findall(x -> x == 1, couplings .!= 0.0)
+    coupling_gates(edges, num_qubits, G)
 end
 
 
 """
-    driver(num_qubits::Int, Gs::Array{Array{T, 1} where T, 1}) 
+    driver(problem::Problem, Gs::Vector{Vector{T} where T}) 
 
 Returns the gates for one driver layer constructed from sums over Kronecker products of two Pauli matrices, 
-e.g., `G==[[X, X], [Y, Y]]` corresponds to ``\\hat X_i \\hat X_j + \\hat Y_i \\hat Y_j`` on all pairs of qubits.       
+e.g., `G==[[X, X], [Y, Y]]` corresponds to ``\\hat X_i \\hat X_j + \\hat Y_i \\hat Y_j`` on all pairs of connected qubits.       
 """
-function driver(num_qubits::Int, Gs::Array{Array{T, 1} where T, 1}) 
-    [coupling_gates(num_qubits, G) for G in Gs] |> chain
+function driver(problem::Problem, Gs::Vector{Vector{T} where T}) 
+    @unpack_Problem problem
+    edges = findall(x -> x == 1, couplings .!= 0.0)
+    [coupling_gates(edges, num_qubits, G) for G in Gs] |> chain
 end
 
 
@@ -77,8 +93,10 @@ e.g. `G==[[X, X]]` corresponds to ``\\hat X_i \\hat X_j``.
 ### Notes
 - This dispatch is simply to enable going from, e.g., `[[X, X], [Y, Y]]` to `[[X, X]]` without having to remove the extra brackets.
 """
-function driver(num_qubits::Int, Gs::Vector{Vector{T}}) where T
-    [coupling_gates(num_qubits, G) for G in Gs] |> chain
+function driver(problem::Problem, Gs::Vector{Vector{T}}) where T
+    @unpack_Problem problem
+    edges = findall(x -> x == 1, couplings .!= 0.0)
+    [coupling_gates(edges, num_qubits, G) for G in Gs] |> chain
 end
 
 
@@ -88,7 +106,7 @@ end
 Returns the circuit corresponding to one QAOA layer.
 """
 function layer(problem::Problem)
-    [objective_function(problem.num_qubits), driver(problem.num_qubits, problem.driver)] |> chain
+    [objective_function(problem.num_qubits), driver(problem, problem.driver)] |> chain
 end
 
 
