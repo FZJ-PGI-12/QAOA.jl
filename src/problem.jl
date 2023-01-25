019fbb8d8195cf7646ab7b998ea1a75470505042
Tim <9047234+timbode@users.noreply.github.com>
2023-01-25 14:09:29 +0100

    3 add option to break z2 symmetry (#4)
    * format code
    
    * add constructor for breaking Z2 symmetry

--- src/problem.jl
+++ src/problem.jl
@@ -18,15 +18,36 @@ $(TYPEDFIELDS)
     "The coupling matrix of the Ising problem Hamiltonian."
     couplings::Matrix{Real}
 
-    "The edges of the graph specified by the coupling matrix"
+    "The edges of the graph specified by the coupling matrix."
     edges
 
     "The driver of the QAOA circuit. By default the Pauli matrix `X`. May also be set to, 
     e.g., `[[X, X], [Y, Y]]` to obtain the drivers``\\hat X_i \\hat X_j + \\hat Y_i \\hat Y_j`` acting on every pair of connected qubits."
     driver
 
-    Problem(num_layers, local_fields, couplings) = new(size(local_fields)[1], num_layers, local_fields, couplings, findall(x -> x == 1, couplings .!= 0.0), X)
-    Problem(num_layers, local_fields, couplings, driver) = new(size(local_fields)[1], num_layers, local_fields, couplings, findall(x -> x == 1, couplings .!= 0.0), driver)
+    # The default constructor
+    Problem(num_layers, local_fields, couplings) = new(size(local_fields)[1], 
+                                                       num_layers, 
+                                                       local_fields, 
+                                                       couplings, 
+                                                       findall(x -> !iszero(x), couplings), 
+                                                       X)
+
+    # Constructor with customized driver
+    Problem(num_layers, local_fields, couplings, driver) = new(size(local_fields)[1], 
+                                                               num_layers, 
+                                                               local_fields, 
+                                                               couplings, 
+                                                               findall(x -> !iszero(x), couplings), 
+                                                               driver)
+
+    # Default constructor explicitly breaking the Z2 symmetry when the local fields are zero
+    Problem(num_layers, couplings) = new(size(couplings)[1] - 1, 
+                                         num_layers, 
+                                         couplings[1:end-1, end], 
+                                         couplings[1:end-1, 1:end-1], 
+                                         findall(x -> !iszero(x), couplings[1:end-1, 1:end-1]), 
+                                         X)
 end
 
 
