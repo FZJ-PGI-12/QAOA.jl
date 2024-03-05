f37d663bd00df6a521ded123d946efbf58251eeb
timbode <lappet@protonmail.com>
2024-03-05 15:50:35 +0100

    closes #50

--- src/annealing.jl
+++ src/annealing.jl
@@ -35,3 +35,32 @@ function anneal(problem::Problem, schedule::Function, T::Float64)
     probabilities = uniform_state(nqubits(circ)) |> circ |> probs
     probabilities
 end
+
+
+"""
+    anneal(problem::Problem, beta::Vector{Float64}, gamma::Vector{Float64})
+
+An extra function to do quantum annealing for a given problem instead of the QAOA.
+
+### Input 
+- `problem::Problem`: A `Problem` instance defining the optimization problem.
+- `beta::Vector{Float64}`: A vector of schedule parameters multiplying the transverse-field or driver Hamiltonian.
+- `gamma::Vector{Float64}`: A vector of schedule parameters multiplying the problem or computational-basis Hamiltonian.
+
+### Output
+- `probabilities::Vector{Float64}`: The vector of output probabilities over the bitstrings.
+
+### Notes
+A good choice for the parameters `beta` and `gamma` is given in Appendix A of https://arxiv.org/abs/1907.02359.
+``
+
+"""
+function anneal(problem::Problem, beta::Vector{Float64}, gamma::Vector{Float64})
+    @unpack_Problem problem
+
+    beta_and_gamma = vcat(beta, gamma)
+    circ = circuit(problem)
+    circ = dispatch_parameters!(circ, problem, beta_and_gamma)
+    probabilities = uniform_state(nqubits(circ)) |> circ |> probs
+    probabilities
+end
